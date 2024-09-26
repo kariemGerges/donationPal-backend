721626862f89824fa69640fab7371bd55926617c
kgerges <kgerges@iu.edu>
2024-09-26 09:54:43 -0400

    Login v1.2

--- donationpalServer.js
+++ donationpalServer.js
@@ -38,7 +38,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-    origin: 'http://localhost:3001',
+    // origin: 'http://localhost:3001',
+    origin: 'https://kariemgerges.github.io/donationPal-frontend/',
     credentials: true
 }));
 app.use(limiter);
